1de39a7463efb6a646eaba4b5324565f5fe3cc42
James Trenholme <jje.trenholme@gmail.com>
2021-04-22 15:33:26 +0100

    added router links and restructured the app to have elements outside the router component

--- pages/dashboard.js
+++
@@ -1,9 +0,0 @@
-import Container from 'react-bootstrap/Container'
-
-export default function Dashboard() {
-	return (
-		<Container className='my-4'>
-			<h1>Welcome to the F1 Hub</h1>
-		</Container>
-	)
-}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,3 @@
-import Head from 'next/head'
-
-import Dashboard from './dashboard'
-import Header from '../components/header'
-
-export default function Home() {
-	return (
-		<div>
-			<Head>
-				<link rel='preconnect' href='https://fonts.gstatic.com'></link>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600&display=swap'
-					rel='stylesheet'></link>
-				<link href='./'></link>
-			</Head>
-			<header className='bg-dark'>
-				<Header />
-			</header>
-			<main>
-				<Dashboard />
-			</main>
-		</div>
-	)
+export default function Dashboard() {
+	return <h1>Welcome to the F1 Hub</h1>
 }
